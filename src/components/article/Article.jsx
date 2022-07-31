d28bd6903f4707047ea2f3302636010e2eb76dca
JSShant <joshua.shanthikumar@outlook.com>
2022-08-01 03:57:29 +1000

    Footer and blog - styling + responsiveness

--- src/components/article/Article.jsx
+++ src/components/article/Article.jsx
@@ -1,10 +1,19 @@
 import React from "react";
 import "./article.css";
 
-const Article = () => {
+const Article = ({ imgUrl, date, text }) => {
   return (
-    <div>
-      <h1></h1>
+    <div className="blog__container--article">
+      <div className="blog__container--article-img">
+        <img src={imgUrl} alt="blog__image" />
+      </div>
+      <div className="blog__container--article-content">
+        <div>
+          <p>{date}</p>
+          <h3>{text}</h3>
+        </div>
+        <p>Read Full Article</p>
+      </div>
     </div>
   );
 };
