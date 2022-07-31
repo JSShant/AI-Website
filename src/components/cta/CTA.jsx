9f8a2abd55ce8326e0af5affb96612604936ee7f
JSShant <joshua.shanthikumar@outlook.com>
2022-08-01 02:39:28 +1000

    Information+Features - Styling + Responsiveness

--- src/components/cta/CTA.jsx
+++ src/components/cta/CTA.jsx
@@ -3,8 +3,14 @@ import "./cta.css";
 
 const CTA = () => {
   return (
-    <div>
-      <h1></h1>
+    <div className="cta">
+      <div className="cta__content">
+        <p>Request Early Access to Get Started</p>
+        <h3>Register Today & start exploring the endless possibilities.</h3>
+      </div>
+      <div className="cta__btn">
+        <button type="button">Get Started</button>
+      </div>
     </div>
   );
 };
--- src/components/feature/Feature.jsx
+++ src/components/feature/Feature.jsx
@@ -1,10 +1,16 @@
 import React from "react";
 import "./feature.css";
 
-const Feature = () => {
+const Feature = ({ title, text }) => {
   return (
-    <div>
-      <h1></h1>
+    <div className="features__container--feature">
+      <div className="features__container--feature-title">
+        <div />
+        <h1>{title}</h1>
+      </div>
+      <div className="features__container--feature-text">
+        <p>{text}</p>
+      </div>
     </div>
   );
 };
